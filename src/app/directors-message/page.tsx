2e3fdd92cf7f26e615688afd4c194940f9c340bc
Manne Rithvik <mannerithvik708@gmail.com>
2024-05-04 19:14:48 +0530

    third commit

--- src/app/directors-message/page.tsx
+++ src/app/directors-message/page.tsx
@@ -9,12 +9,13 @@ export default function DirectorMessage(){
      </div>
      <div className="message-text">
         
-        <br/>
-        <p>Founded in 2008, the Indian Institute of Technology Hyderabad (IITH) has established a rich tradition of excellence across a broad range of academic disciplines. IITH is one of India's fastest-growing, forward-thinking institutions among the Higher Education and Research institutions with an international presence. IITH is committed to providing high-quality education without geographical boundaries, bringing diversity to its student body and research activities. IITH creates a unique holistic ecosystem for education that offers interactive learning, a very flexible academic structure, cutting-edge research, strong industry collaboration, and entrepreneurship. IITH has a very vibrant innovation eco-system and passionately follows its philosophy, Inventing and Innovating in Technology for Humanity (IITH). Our vision finds us in an academic and professional world without boundaries. We are forming strategic partnerships around the world to attain our goals of Internationalisation.
-<br/>
-<br/>
-IITH enjoys a very special relationship with Japanese universities and industries that goes beyond academics and research collaborations. We are also interested to initiate bilateral partnerships with Universities and Research organisations of repute, which will benefit both students and the faculty community.</p></div>    
-
+        <br/><p>
+                        Founded in 2008, the Indian Institute of Technology Hyderabad (IITH) has established a rich tradition of excellence across a broad range of academic disciplines. IITH is one of India&apos;s fastest-growing, forward-thinking institutions among the Higher Education and Research institutions with an international presence. IITH is committed to providing high-quality education without geographical boundaries, bringing diversity to its student body and research activities. IITH creates a unique holistic ecosystem for education that offers interactive learning, a very flexible academic structure, cutting-edge research, strong industry collaboration, and entrepreneurship. IITH has a very vibrant innovation eco-system and passionately follows its philosophy, Inventing and Innovating in Technology for Humanity (IITH). Our vision finds us in an academic and professional world without boundaries. We are forming strategic partnerships around the world to attain our goals of Internationalisation.
+                        <br />
+                        <br />
+                        IITH enjoys a very special relationship with Japanese universities and industries that goes beyond academics and research collaborations. We are also interested to initiate bilateral partnerships with Universities and Research organisations of repute, which will benefit both students and the faculty community.
+                    </p>
+    </div>
     </div>
     </div>
     )
