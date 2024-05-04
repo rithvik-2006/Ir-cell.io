7df0ac9ba023297d0226631c69338e8e2722a386
Manne Rithvik <mannerithvik708@gmail.com>
2024-05-04 18:01:14 +0530

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Navbar from "./nav-bar/page";
 import Navbar2 from "./nav-bar2/page";
-import ImageCarousel from "./image-carousel/page";
+
 import DirectorMessage from "./directors-message/page";
 import IntroVid from "./intro-video/page";
 import Footer from "./footer/page";
@@ -9,7 +9,6 @@ export default function Home() {
   return (<div className="home-body">
     <Navbar/>
     <Navbar2/>
-    <ImageCarousel/>
     <DirectorMessage/>
     <IntroVid/>
     <Footer/>
